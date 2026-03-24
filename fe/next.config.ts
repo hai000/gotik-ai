7ac06a84bfaaa4110543bbc44808407e38e76a6d
NTH <21130343@st.hcmuaf.edu.vn>
2026-03-24 15:52:16 +0700

    feat: add global styles and layout for Next.js app with dark mode support

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
